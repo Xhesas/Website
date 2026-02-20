559c76c32d17a33b0602bf6477bd3e76534dd060
Xhesas <namnak.xhesas@gmail.com>
2026-02-20 01:55:47 +0100

    Initial Commit

---
+++ main/__init__.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, make_response
+from werkzeug.utils import send_file
+from werkzeug.exceptions import HTTPException
+import numpy as np
+
+ERR = [
+    {"code": 400,"text": ["Bad request!"]},
+    {"code": 403,"text": ["You have been blocked!"]},
+    {"code": 404,"text": ["Page not found!"]},
+    {"code": 411,"text": ["Length required!"]},
+    {"code": 413,"text": ["Too large!"]},
+    {"code": 418,"text": ["I'm a teapot!"]},
+    {"code": 500,"text": ["An internal error has occured!"]},
+    {"code": 501,"text": ["This page has not been implemented!"]},
+    {"code": 507,"text": ["The server has not found sufficient storage for your request!"]}
+]
+
+app = Flask(__name__)
+
+@app.route('/')
+def route_index():
+    return render_template('index.html')
+
+@app.route('/style')
+@app.route('/style.css')
+def route_style():
+    return send_file('style.css', request.environ)
+
+@app.route('/about')
+@app.route('/about.html')
+def route_about():
+    return render_template('about.html')
+
+@app.route('/sitemap.xml')
+def route_sitemap():
+    return send_file('robots/sitemap.xml', request.environ)
+
+@app.route('/sitemap-index.xml')
+def route_sitemap_index():
+    return send_file('robots/sitemap-index.xml', request.environ)
+
+@app.route('/robots.txt')
+def route_robots():
+    return send_file('robots/robots.txt', request.environ)
+
+@app.errorhandler(HTTPException)
+def route_exception(e):
+    a = [a for a in ERR if a['code'] == e.code]
+    msg = a[0]['text'] if a else ["An Error has occurred!"]
+    return make_response(render_template(
+        'error.html',
+        code=e.code,
+        msg=np.random.choice(msg, p=np.arange(len(msg), 0, -1)/np.arange(len(msg), 0, -1).sum())
+    ), e.code)
+
+if __name__ == "__main__":
+    app.run(port=5000)
